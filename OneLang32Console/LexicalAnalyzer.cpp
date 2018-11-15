b7d8e258bad1da70e8da204ac3cda2b9eec881d7
bg1bgst333 <bg1bgst333@gmail.com>
2018-11-15 16:39:50 +0900

    -ファイルの読み込み.

--- OneLang32Console/LexicalAnalyzer.cpp
+++ OneLang32Console/LexicalAnalyzer.cpp
@@ -3,6 +3,7 @@
 #include <iostream>	// C++標準入出力
 // 独自のヘッダ
 #include "LexicalAnalyzer.h"	// CLexicalAnalyzer
+#include "TextFile.h"	// CTextFile
 
 // 解析を実行するメンバ関数Analyze.
 BOOL CLexicalAnalyzer::Analyze(tstring tstrSourceFileName){
@@ -15,6 +16,18 @@ BOOL CLexicalAnalyzer::Analyze(tstring tstrSourceFileName){
 
 	// "CLexicalAnalyzer::Analyze!"と表示.
 	std::wcout << _T("CLexicalAnalyzer::Analyze!") << std::endl;	// "CLexicalAnalyzer::Analyze!"を出力.
+
+	// ソースコードファイルを読み込み, 出力.
+	CTextFile *pTextFile = new CTextFile();	// CTextFileオブジェクトを作成し,  ポインタをpTextFileに格納.
+	if (pTextFile->Read(m_tstrSourceFileName.c_str())){	// pTextFile->Readで読み込み.
+		std::wcout << _T("----- start -----") << std::endl;	// "----- start -----"と出力.
+		std::wcout << pTextFile->m_tstrText << std::endl;	// pTextFile->m_tstrTextの内容を出力.
+		std::wcout << _T("----- end -----") << std::endl;	// "----- start -----"と出力.
+	}
+	else{	// 読み込み失敗.
+		std::wcout << _T("Read Failure!") << std::endl;	// "Read Failure!"と出力.
+	}
+	delete pTextFile;	// pTextFileを解放.
 
 	// 戻り値を返す.
 	return TRUE;	// TRUEを返す.
